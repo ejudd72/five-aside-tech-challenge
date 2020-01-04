973651e9a91cc3aa6a9d09c84337c59d6ad876e1
Eleanor <e.e.j@live.co.uk>
2020-01-04 15:48:17 +0000

    some refactoring to component and prettifying

--- 5-aside/src/components/Pitch/Pitch.js
+++ 5-aside/src/components/Pitch/Pitch.js
@@ -16,18 +16,17 @@ const Pitch = ({
 }) => ( 
     !submitted || showPrevious || showAbout ? null : (
     <div className="container">
-
         <h3>Players on pitch:</h3>
-        <ul className="all-player-list" style={{ display: "flex" }}>
+        <ul className="all-player-list">
             { allPlayers.map((current, index) => (
-                <li className="all-player-list-item" key={ index }>{ current.name }</li>
+                <li className="all-player-list-item" key={ index }>
+                    { current.name }
+                </li>
             ))}
         </ul>
 
         <h3>Sorted: { randomSort === true ? "Randomly" : "Based on skill level"}</h3>
-        <section 
-            className="full-pitch"
-        > 
+        <section className="full-pitch"> 
             <Teams
                 teamNumber={ 1 }
                 team={ team1 }
@@ -41,22 +40,21 @@ const Pitch = ({
                 teamName={ teamNames[1] }
                 shirtChoice={ shirtChoice[1]}
             />
-            </section>
-            <div className="button-panel">
-                <button
-                    className="pitch button"
-                    onClick={ handleEditPlayers }> Edit Players
-                </button>
-                <button
-                    className="pitch button"
-                    onClick={ handleReset }> Reset Teams
-                </button>
-            </div>
-            
+
+        </section>
+        <div className="button-panel">
+            <button
+                className="pitch button"
+                onClick={ handleEditPlayers }> Edit Players
+            </button>
+            <button
+                className="pitch button"
+                onClick={ handleReset }> Reset Teams
+            </button>
         </div>
+        
+    </div>
     )
 )
-    
-    
 
 export default Pitch;
