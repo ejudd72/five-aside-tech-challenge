3fb334b4ddcb7e61aaaa59a99eb91476255bc1a7
Eleanor <e.e.j@live.co.uk>
2020-01-04 16:23:56 +0000

    Updates

---
+++ precache-manifest.deae4f63d7b86853949f2e78a495558d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f04969f7041086a48cc9d579a653b5",
+    "url": "/five-aside-tech-challenge/index.html"
+  },
+  {
+    "revision": "41dd119de9be93dce332",
+    "url": "/five-aside-tech-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4036acca4a76f4c5a3d6",
+    "url": "/five-aside-tech-challenge/static/js/2.b83b97d5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/five-aside-tech-challenge/static/js/2.b83b97d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41dd119de9be93dce332",
+    "url": "/five-aside-tech-challenge/static/js/main.12e5bb3c.chunk.js"
+  },
+  {
+    "revision": "8deb04749609ca56b888",
+    "url": "/five-aside-tech-challenge/static/js/runtime-main.945a02e5.js"
+  }
+]);
